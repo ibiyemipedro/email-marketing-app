40fd76ca746fca5cb0dd1ec6f9dc90b8961e279b
Ibiyemi Pedro <36103734+ibiyemipedro@users.noreply.github.com>
2021-02-13 15:22:54 +0100

    Delete emailParams.js

--- auth/emailParams.js
+++
@@ -1,7 +0,0 @@
-const emailParams = {
-    from: '"Upbase Admin" <admin@upbase.com.ng>', // sender address
-    to: 'yemipedro07@gmail.com', // list of receivers
-    subject: 'Tech Accelerator Program',
-}
-
-module.exports = emailParams;
